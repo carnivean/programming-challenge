fe2ff4e835766812d61b819027db8543b02a1e0c
Erik <erikkynast@gmail.com>
2021-06-02 22:02:39 +0200

    QuickAndDirty Solution
    Quick and Dirty solution that solves both problem statements, without any real regard towards maintainablitiy, reusability.
    I think the readability is kinda ok, beased on the real small scope of the problem (The solution is literally a stream with some mapping/filtering).

--- src/main/java/de/exxcellent/challenge/QnDData.java
+++ src/main/java/de/exxcellent/challenge/QnDData.java
@@ -1,2 +1,19 @@
-package de.exxcellent.challenge;public class QnDData {
+package de.exxcellent.challenge;
+
+/*
+    Hab geschaut, ob man das mit Java Records machen kann, dann müsste man aber die Berechnung von absoluteDifference
+    auslagern, was ich für keine gute Idee halte, da es logisch zu der Datenhaltung gehört
+ */
+public class QnDData {
+    public int max, min;
+    public String result;
+    public int absoluteDiff;
+
+    public QnDData(int max, int min, String result) {
+        this.max = max;
+        this.min = min;
+        this.result = result;
+
+        this.absoluteDiff = Math.abs(max - min);
+    }
 }
---
+++ src/main/java/de/exxcellent/challenge/QuickAndDirty.java
@@ -0,0 +1,43 @@
+package de.exxcellent.challenge;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+public class QuickAndDirty {
+    public static String processCSVFile(String fileName, Function<String, QnDData> mapperFunction) {
+        var filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\de\\exxcellent\\challenge\\" + fileName;
+        String result = "";
+        try {
+            result = Files.lines(Paths.get(filePath))
+                    .skip(1)
+                    .map(mapperFunction)
+                    .min((o1, o2) -> o1.absoluteDiff - o2.absoluteDiff)
+                    .get()
+                    .result;
+        } catch (IOException e) {
+            /*
+            Hier könnte man eine vernünftige Fehlerbehandlung machen, aber wenn die Datei fehlt, ist die Aufgabe in beiden
+            Fällen nicht lösbar. Da gibt es keine wirkliche sinnvolle Fehlerbehandlung.
+             */
+            System.err.println("Die Datei scheint zu fehlen.");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /*
+    Lamdas :)
+     */
+    public static Function<String, QnDData> mapWeatherData = (line) -> {
+        String[] td = line.split(",");
+        return new QnDData(Integer.parseInt(td[1]), Integer.parseInt(td[2]), td[0]);
+    };
+
+    public static Function<String, QnDData> mapFootballData = (line) -> {
+        String[] td = line.split(",");
+        return new QnDData(Integer.parseInt(td[5]), Integer.parseInt(td[6]), td[0]);
+    };
+}
